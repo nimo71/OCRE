66c1f217375938d73a71a07e73ad32bb2fafb636
Nick Monk <nick@monksoftware.co.uk>
2012-02-19 23:02:52 +0000

    initial interested bookmarklet added to profile page

---
+++ app/controllers/InterestController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api._
+import play.api.libs.json._
+import play.api.mvc._  	
+  	
+object InterestController extends Controller {
+  
+  	def interested(id: String) = Action { implicit request => 
+  		Logger.info("Interested, id="+ id +", url="+ request.queryString("u")(0))
+  		Ok(Json.toJson(Map("result"->"Ok")))
+  	}
+}
--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,18 +1,10 @@
 package controllers
 
-import play.api._
-import play.api.i18n._
 import play.api.mvc._
-import play.api.data._
-import play.api.data.Form._
-import play.api.data.Forms._
-import play.api.data.validation.Constraints._
-  	
 
 object UserController extends Controller {
   
   	import views._
-  	import models._
   	
   	def profile() = Action { implicit request => 
   		Ok(html.profile())
