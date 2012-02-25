05f540e0ff9f103159ce4cc47d537f084c8d7f45
Nick Monk <nick@monksoftware.co.uk>
2012-02-25 01:39:33 +0000

    Factored out reset password functionality into ResetPasswordController Split profile page into a list of interests and bookmarklet column Added static list of interests to the user profile page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,12 +15,6 @@ object Application extends Controller {
   	import views._
   	import models._
   
-  	val email = text verifying pattern(
-  		"""\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b""".r,
-  		"constraint.email",
-  		"error.invalid.email"
-	)
-  
   	/**
   		Log in form backing object
   	*/
@@ -63,36 +57,5 @@ object Application extends Controller {
       		}
   		)
     }  
-    
-  	/**
-  		Log in form backing object
-  	*/
-  	val forgottenPasswordForm = Form(
-    		"email" -> email
-  	)    
-    
-    /**
-    	Show the forgotten password form
-     */
-    def forgottenPassword() = Action { implicit request => 
-    	Ok(html.forgottenPassword(forgottenPasswordForm))
-    }
-    
-    /**
-    	Process the forgotten password form
-     */
-    def resetPassword() = Action { implicit request => 
-    	forgottenPasswordForm.bindFromRequest.fold(
-	    	// Form has errors, redisplay it
-      		errors => BadRequest(html.forgottenPassword(errors)),
-      
-      		// We got a valid User value, display the summary
-      		user => {
-      			// save the new user to the database
-      			Redirect(routes.Application.index)
-      				.flashing("message" -> Messages("password.reset"))
-      		}
-      	)
-    }
     
 }
