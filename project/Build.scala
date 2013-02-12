6015df8e0f6259daea5feea02ea767b23533e54d
Nick Monk <nick@monksoftware.co.uk>
2013-02-12 21:15:55 +0000

    Update to play 2.1.0

--- project/Build.scala
+++ project/Build.scala
@@ -1,18 +1,25 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "ocre"
-    val appVersion      = "1.0"
+  val appName         = "ocre"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    jdbc,
+    anorm
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies).settings(defaultScalaSettings:_*).settings(
-      // Add your own project settings here      
-    )
+
+//  resolvers += "repo.codahale.com" at "http://repo.codahale.com"
+//  libraryDependencies += "com.codahale" % "jerkson_2.9.1" % "0.5.0"
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
 
 }
